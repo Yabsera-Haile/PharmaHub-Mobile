12df9db34d5c02a2a3bfa0f703a7d9c854345366
Yabsera Yemanberhan <haileyabsera3@gmail.com>
2024-03-09 09:19:05 +0300

    Temporary Data

---
+++ data/catagories.js
@@ -0,0 +1,46 @@
+export default catagories= [
+    {
+        id:1,
+        name:"Antibiotics",
+        image: require("../assets/Images/Catagories/antibiotics.jpeg")
+    },
+    {
+        id:2,
+        name: "Pain Killers",
+        image: require("../assets/Images/Catagories/painkillers.jpeg")
+    },
+    {
+        id:3,
+        name: "Vitamins",
+        image: require("../assets/Images/Catagories/vitamins.jpeg")
+    },
+    {
+        id:4,
+        name: "Cough & Cold",
+        image: require("../assets/Images/Catagories/coughcold.jpeg")
+    },
+    {
+        id:5,
+        name: "Skin Care",
+        image: require("../assets/Images/Catagories/skincare.jpeg")
+    },
+    {
+        id:6,
+        name: "First Aid",
+        image: require("../assets/Images/Catagories/firstaid.png")
+    },
+    {
+        id:7,
+        name: "Oral Care",
+        image: require("../assets/Images/Catagories/oralcare.jpeg")
+    },
+    {
+        id:8,
+        name: "Hair Care",
+        image: require("../assets/Images/Catagories/haircare.jpeg")
+    },
+    {
+        id:9,
+        name: "Eye Care",
+        image: require("../assets/Images/Catagories/eyecare.jpeg")
+    }]
